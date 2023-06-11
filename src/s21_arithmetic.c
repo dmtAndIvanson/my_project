c52a65861a2550084fbdd44f2d28d58856edadf7
dmitry <dmt.and.ivanson@gmail.com>
2023-06-11 21:03:06 +0300

    Updated bit summator

--- src/s21_arithmetic.c
+++ src/s21_arithmetic.c
@@ -1,17 +1,19 @@
 #include "s21_decimal.h"
 
+#define NUMBER_BETWEEN(number, left, right) ((number) >= (left) && (number) <= (right))
+
 void s21_sum_bits(int val_1[BUFF_SIZE], int val_2[BUFF_SIZE], int res[BUFF_SIZE])
 {
     int min = 0;
     int max = 66;
-    if ((val_1[0] >= min && val_1[0] <= max) && (val_2[0] >= min && val_2[0] <= max))
+    if (val_1 && val_2 && NUMBER_BETWEEN(val_1[0], min, max) && NUMBER_BETWEEN(val_2[0], min, max))
     {
         int setter = 1;
         int remember_bit = 0;
-        for (int i = BUFF_SIZE-1; i > 0; i--)
+        for (int i = BUFF_SIZE-1; i >= 0; i--)
         {
             int tmp_res = 0;
-            for (int j = 0; j < 8; j++)
+            for (size_t j = 0; j < sizeof(int)*8-1; j++)
             {
                 int sum = ((val_1[i] & (setter << j)) >> j) + ((val_2[i] & (setter << j)) >> j) + remember_bit;
                 tmp_res |= ((sum % 2) << j);
